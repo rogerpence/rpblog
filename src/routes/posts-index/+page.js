1c26c565bfb09ca1c5a8aa56bd26e00ed56eefaa
Roger Pence <roger.pence@gmail.com>
2024-01-19 17:14:03 -0600

    remove still posts-index folder

--- src/routes/posts-index/+page.js
+++
@@ -1,8 +0,0 @@
-export const prerender = true;
-
-export async function load({ fetch }) {
-	const response = await fetch('api/posts');
-	const posts = await response.json();
-
-	return { posts };
-}
